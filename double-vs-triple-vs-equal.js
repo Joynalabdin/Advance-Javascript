c0e28144c839f765b030883a86513572a6a5625d
joynal abdin robel <Joynalabdin>
2020-03-05 21:43:03 +0600

    imortant interview question

---
+++ double-vs-triple-vs-equal.js
@@ -0,0 +1,22 @@
+// const first = 2;
+// const second = 5;
+// if (first == second){
+//     console.log("condition is true");
+// }
+// else {
+//     console.log("condition is flase");
+// }
+
+const first = 1;
+const second = true;
+if(first == second){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is flase")
+}
+
+// Important question for interview
+
+// Double equal check(==) only value type.
+// Triple equal check(===) value and data type.
