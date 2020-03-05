4f31006a01ae57a587c7c0b5b7672ce44d84e21e
joynal abdin robel <Joynalabdin>
2020-03-05 19:46:37 +0600

    true-flase

---
+++ true-flase.js
@@ -0,0 +1,23 @@
+const name = [];
+
+if(name){
+    console.log("condition is true")
+}
+ else{
+     console.log("condition is flase")
+ }
+
+
+
+
+// if value is any number but no argument then the condition is true.
+// Any string is true, any white space is true.
+// Empty [] array is true.
+// 
+
+// flase value
+// Zero is flase
+// Empty string is flase.
+// Any undifine is flase.
+// null is also flase.
+// NaN is also flase.
